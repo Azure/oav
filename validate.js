6883f91690b7cf36661c3af38c3dd1fa509e03f1
Amar Zavery <amzavery@microsoft.com>
2017-01-19 20:56:42 -0800

    liveTest support, foundation work to merge allOf and set additionalProperties to false

--- validate.js
+++ validate.js
@@ -3,9 +3,14 @@
 
 'use strict';
 
-var log = require('./lib/util/logging'),
+var msrest = require('ms-rest'),
+  msrestazure = require('ms-rest-azure'),
+  ResourceManagementClient = require('azure-arm-resource').ResourceManagementClient,
+  log = require('./lib/util/logging'),
   utils = require('./lib/util/utils'),
+  Constants = require('./lib/util/constants'),
   path = require('path'),
+  util = require('util'),
   SpecValidator = require('./lib/specValidator');
 
 exports.finalValidationResult = { validityStatus: true };
@@ -42,7 +47,7 @@ exports.validateSpec = function validateSpec(specPath, json) {
   let validator = new SpecValidator(specPath);
   exports.finalValidationResult[specPath] = validator.specValidationResult;
   validator.initialize().then(function() {
-    log.info(`\n> Semantically validating  ${specPath}:\n`);
+    log.info(`Semantically validating  ${specPath}:\n`);
     validator.validateSpec();
     exports.updateEndResultOfSingleValidation(validator);
     exports.logDetailedInfo(validator, json);
@@ -53,20 +58,12 @@ exports.validateSpec = function validateSpec(specPath, json) {
   });
 };
 
-exports.executeSequentially = function executeSequentially(promiseFactories) {
-  var result = Promise.resolve();
-  promiseFactories.forEach(function (promiseFactory) {
-    result = result.then(promiseFactory);
-  });
-  return result;
-};
-
 exports.validateCompositeSpec = function validateCompositeSpec(compositeSpecPath, json){
   return exports.getDocumentsFromCompositeSwagger(compositeSpecPath).then(function(docs) {
     let promiseFactories = docs.map(function(doc) {
       return exports.validateSpec(doc, json);
     });
-    return exports.executeSequentially(promiseFactories);
+    return utils.executePromisesSequentially(promiseFactories);
   }).catch(function (err) {
     log.error(err);
   });
@@ -76,7 +73,7 @@ exports.validateExamples = function validateExamples(specPath, operationIds, jso
   let validator = new SpecValidator(specPath);
   exports.finalValidationResult[specPath] = validator.specValidationResult;
   validator.initialize().then(function() {
-    log.info(`\n> Validating "examples" and "x-ms-examples" in  ${specPath}:\n`);
+    log.info(`Validating "examples" and "x-ms-examples" in  ${specPath}:\n`);
     validator.validateOperations(operationIds);
     exports.updateEndResultOfSingleValidation(validator);
     exports.logDetailedInfo(validator, json);
@@ -91,7 +88,7 @@ exports.validateExamplesInCompositeSpec = function validateExamplesInCompositeSp
     let promiseFactories = docs.map(function(doc) {
       return exports.validateExamples(doc, json);
     });
-    return exports.executeSequentially(promiseFactories);
+    return utils.executePromisesSequentially(promiseFactories);
   }).catch(function (err) {
     log.error(err);
   });
@@ -107,7 +104,7 @@ exports.updateEndResultOfSingleValidation = function updateEndResultOfSingleVali
     exports.finalValidationResult.validityStatus = validator.specValidationResult.validityStatus;
   }
   return;
-}
+};
 
 exports.logDetailedInfo = function logDetailedInfo(validator, json) {
   if (json) {
@@ -120,6 +117,106 @@ exports.logDetailedInfo = function logDetailedInfo(validator, json) {
     log.silly('############################');
     log.silly(validator.specValidationResult);
     log.silly('----------------------------');
+  }
+};
+
+exports.sanitizeParameters = function sanitizeParameters(exampleParameters) {
+  if (exampleParameters) {
+    if (exampleParameters.subscriptionId) {
+      exampleParameters.subscriptionId = process.env['AZURE_SUBSCRIPTION_ID'];
+    }
+    if (exampleParameters.resourceGroupName) {
+      exampleParameters.resourceGroupName = process.env['AZURE_RESOURCE_GROUP'];
+    }
+  }
+  return exampleParameters;
+};
+
+exports.liveTest = function liveTest(specPath, operationId) {
+  exports.validateEnvironmentVariables();
+  log.transports.console.level = 'info';
+  let clientId = process.env['CLIENT_ID'];
+  let domain = process.env['DOMAIN'];
+  let secret = process.env['APPLICATION_SECRET'];
+  let subscriptionId = process.env['AZURE_SUBSCRIPTION_ID'];
+  let location = process.env['AZURE_LOCATION'] || 'westus';
+  let resourceGroupName = process.env['AZURE_RESOURCE_GROUP'] || utils.generateRandomId('testrg');
+  let validator = new SpecValidator(specPath);
+  let operation, xmsExamples;
+  validator.initialize().then(function() {
+    log.info(`Running live test using x-ms-examples in ${operationId} in  ${specPath}:\n`);
+    operation = validator.getOperationById(operationId);
+    xmsExamples = operation[Constants.xmsExamples];
+    if (xmsExamples) {
+      for (let scenario in xmsExamples) {
+        let xmsExample = xmsExamples[scenario];
+        let parameters = exports.sanitizeParameters(xmsExample.parameters);
+        let result = validator.validateRequest(operation, xmsExample.parameters);
+        if (result.validationResult && result.validationResult.errors && result.validationResult.errors.length) {
+          let msg = `Cannot proceed ahead with the live test for operation: ${operationId}. Found validation errors in the request.\n` + 
+            `${util.inspect(result.validationResult.errors, {depth: null})}`;
+          throw new Error(msg);
+        }
+        let req = result.request;
+        if (!req) {
+          throw new Error(`Cannot proceed ahead with the live test for operation: ${operationId}. The request object is undefined.`);
+        }
+        if (req.body !== null && req.body !== undefined) {
+          req.body = JSON.stringify(req.body);
+        }
+        msrestazure.loginWithServicePrincipalSecret(clientId, secret, domain, function(err, creds, subscriptions) {
+          if (err) {
+            throw err;
+          }
+          let resourceClient = new ResourceManagementClient(creds, subscriptionId);
+          let client = new msrestazure.AzureServiceClient(creds);
+          exports.createResourceGroup(resourceClient, location, resourceGroupName, function(err, resourceGroup) {
+            if (err) {
+              throw err;
+            }
+            client.sendRequest(req, function(err, result, request, response) {
+              log.info(request);
+              log.info(response);
+              if (err) {
+                throw err;
+              }
+              log.info(result);
+            });
+          });
+        });
+      }
+    }
+
+  }).catch(function (err) {
+    log.error(err);
+  });
+};
+
+exports.createResourceGroup = function createResourceGroup(resourceClient, location, resourceGroupName, callback) {
+  resourceClient.resourceGroups.get(resourceGroupName, function (err, result, request, response) {
+    if (err && err.statusCode === 404) {
+      log.info(`Creating resource group: \'${resourceGroupName}\', if not present.`);
+      let groupParameters = { location: location, tags: { 'live-test': 'live-test'} };
+      return resourceClient.resourceGroups.createOrUpdate(resourceGroupName, groupParameters, callback);
+    } else {
+      return callback(null, result);
+    }
+  });
+};
+
+exports.getResourceGroup = function getResourceGroup(resourceClient, resourceGroupName, callback) {
+  log.info(`Searching ResourceGroup: ${resourceGroupName}.`)
+  resourceClient.resourceGroups.get(resourceGroupName, callback);
+}
+
+exports.validateEnvironmentVariables = function validateEnvironmentVariables() {
+  var envs = [];
+  if (!process.env['CLIENT_ID']) envs.push('CLIENT_ID');
+  if (!process.env['DOMAIN']) envs.push('DOMAIN');
+  if (!process.env['APPLICATION_SECRET']) envs.push('APPLICATION_SECRET');
+  if (!process.env['AZURE_SUBSCRIPTION_ID']) envs.push('AZURE_SUBSCRIPTION_ID');
+  if (envs.length > 0) {
+    throw new Error(util.format('please set/export the following environment variables: %s', envs.toString()));
   }
 };
 
