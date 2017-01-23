amarzavery/openapi-validation-tools

resloving allOf

lib/specValidator.js
@@ -3,17 +3,16 @@
 
 'use strict';
 
-var Sway = require('sway'),
-  util = require('util'),
-  msRest = require('ms-rest'),
-  HttpRequest = msRest.WebResource,
+var util = require('util'),
   path = require('path'),
   fs = require('fs'),
+  Sway = require('sway'),
+  msRest = require('ms-rest'),
+  HttpRequest = msRest.WebResource,
   utils = require('./util/utils'),
   Constants = require('./util/constants'),
   log = require('./util/logging'),
   ResponseWrapper = require('./responseWrapper'),
-  jsonRefs = require('json-refs'),
   ErrorCodes = Constants.ErrorCodes;
 
 class SpecValidator {
@@ -47,51 +46,113 @@ class SpecValidator {
 
   findObject(what, where, actualReference, docPath) {
     let self = this;
-    let result = eval(where[what]);
+    let result = eval(`where.${what}`);
     if (!result) {
       let msg = `Object '${what}' from the given reference ` + 
       `'${actualReference}' is not found in the swagger spec '${docPath}'.`;
       let e = self.constructErrorObject('OBJECT_NOT_FOUND', msg);
-      return Promise.reject(e);
+      log.error(e);
+      throw e;
     } else {
-      return Promise.resolve(result);
+      return result;
     }
   }
 
   getDefinitionFromReference(reference) {
     let self = this;
-    if (!reference || (reference && reference.trim().length > 0)) {
+    if (!reference || (reference && reference.trim().length === 0)) {
       throw new Error('reference cannot be null or undefined and it must be a non-empty string.');
     }
     let refObj = utils.parseReferenceInSwagger(reference);
-    let result;
     if (refObj.filePath) {
       let docPath = utils.joinPath(self.specDir, refObj.filePath);
-      utils.parseJson(docPath).then(function(result) {
+      return utils.parseJson(docPath).then(function(result) {
         return self.findObject(refObj.localReference.accessorProperty, result, refObj.localReference.value, docPath);
-      }).catch(function (err) {
-        return Promise.reject(err);
       });
     } else {
       return self.findObject(refObj.localReference.accessorProperty, self.specInJson, refObj.localReference.value, self.specPath);
     }
-}
+  }
+
+  mergeResolvedAllOfObjects(source, target) {
+    let self = this;
+    if (!source || (source && typeof source !== 'object')) {
+      return Promise.reject(new Error(`source must be of type "object".`));
+    }
+    if (!target || (target && typeof target !== 'object')) {
+      return Promise.reject(new Error(`target must be of type "object".`));
+    }
+    //merge the target model's properties
+    source.properties = utils.merge(source.properties, target.properties);
+    //merge the array of required properties
+    if (target.required) {
+      if (!source.required) {
+        source.required = [];
+      }
+      source.required = [...new Set([...source.required, ...target.required])];
+    }
+    //merge x-ms-azure-resource
+    if (target['x-ms-azure-resource']) {
+      source['x-ms-azure-resource'] = target['x-ms-azure-resource'];
+    }
+    return Promise.resolve(source);
+  }
+
+  getRefDefinitionInAllOf(item) {
+    let self = this;
+    if (!item || (item && typeof item !== 'object')) {
+      return Promise.reject(new Error(`item must be of type "object".`));
+    }
+    if (item['$ref']) {
+      return Promise.resolve(self.getDefinitionFromReference(item['$ref']));
+    } else {
+      return Promise.resolve(item);
+    }
+  }
+
+  composeAllOf(model, item) {
+    let self = this;
+    return self.getRefDefinitionInAllOf(item).then(function(result) {
+      if (result && result.allOf) {
+        return self.resolveAllOf(result).then(function(res) {
+          return Promise.resolve(self.mergeResolvedAllOfObjects(model, result));
+        }).catch(function (err) {
+          log.error(err);
+          return Promise.reject(err);
+        });
+      } else {
+        return Promise.resolve(self.mergeResolvedAllOfObjects(model, result));
+      }
+    });
+  }
+
+  resolveAllOf(model) {
+    let self = this;
+    if (!model || (model && typeof model !== 'object')) {
+      return Promise.reject(new Error(`model cannot be null or undefined and must of type object.`));
+    }
+    if (model.allOf) {
+      let mpf = model.allOf.map(function(item) {
+        return Promise.resolve(self.composeAllOf(model, item));
+      });
+      return utils.executePromisesSequentially(mpf);
+    }
+    return Promise.resolve(model);
+  }
 
   makeModelsStricter() {
     let self = this;
     let spec = self.specInJson;
-    for (let model in spec) {
-      if (spec[model] && spec[model].allOf) {
-        spec.model.allOf.forEach(function(item) {
-          self.getDefinitionFromReference(item).then(function(result) {
-            //do something with this model.
-          }).catch(function(err) {
-            return Promise.reject(err);
-          })
-        });
+    let definitions = spec.definitions;
+    let modelNames = Object.keys(self.specInJson.definitions);
+    let mpf = modelNames.map(function (modelName) {
+      let model = definitions[modelName];
+      if (model && !model.additionalProperties) {
+        model.additionalProperties = false;
       }
-    }
-    return;
+      return self.resolveAllOf(model);
+    });
+    return utils.executePromisesSequentially(mpf);
   }
 
   updateValidityStatus(value) {
@@ -124,10 +185,13 @@ class SpecValidator {
       self.unifyXmsPaths();
       return self;
     }).then(function () {
-      let options = {subDocPath: '#/definitions'};
-      return jsonRefs.findRefs(self.specInJson, options);
-    }).then(function(res) {
-      console.log(res);
+      return self.makeModelsStricter();
+    }).then(function() {
+      return Object.keys(self.specInJson.definitions).map(function(definitionName){
+        if (self.specInJson.definitions[definitionName].allOf) {
+          delete self.specInJson.definitions[definitionName].allOf;
+        }
+      });
     }).then(function() {
       let options = {};
       options.definition = self.specInJson;
@@ -139,6 +203,7 @@ class SpecValidator {
       self.swaggerApi = api;
       return Promise.resolve(api);
     }).catch(function (err) {
+      console.dir(err);
       let e = self.constructErrorObject(ErrorCodes.ResolveSpecError, err.message, [err]);
       self.specValidationResult.resolveSpec = e;
       log.error(`${ErrorCodes.ResolveSpecError}: ${err.message}.`);
@@ -506,20 +571,36 @@ class SpecValidator {
     if (exampleResponseValue === null || exampleResponseValue === undefined || typeof exampleResponseValue !== 'object') {
       throw new Error('operation cannot be null or undefined and must be of type \'object\'.');
     }
-
+    let responsesInSwagger = {};
+    let responses = operation.getResponses().map(function(response) { 
+      responsesInSwagger[response.statusCode] = response.statusCode; 
+      return response.statusCode; 
+    });
     for (let exampleResponseStatusCode in exampleResponseValue) {
       let response = operation.getResponse(exampleResponseStatusCode);
-      result[exampleResponseStatusCode] = {errors: [], warnings: []};
+      if (responsesInSwagger[exampleResponseStatusCode]) delete responsesInSwagger[exampleResponseStatusCode];
+      result[exampleResponseStatusCode] = { errors: [], warnings: [] };
+      //have to ensure how to map negative status codes to default. There have been several issues filed in the Autorest repo, w.r.t how
+      //default is handled. While solving that issue, we may come up with some extension. Once that is finalized, we should code accordingly over here.
       if (!response) {
-        let msg = `${exampleResponseStatusCode} for operation ${operation.operationId} is provided in exampleResponseValue, however it is not present in the swagger spec.`;
-        let e = new Error(msg);
-        e.code = ErrorCodes.ResponseStatusCodeNotInSpec;
+        let msg = `Response statusCode "${exampleResponseStatusCode}" for operation "${operation.operationId}" is provided in exampleResponseValue, ` + 
+        `however it is not present in the swagger spec.`;
+        let e = self.constructErrorObject(ErrorCodes.ResponseStatusCodeNotInSpec, msg);
         result[exampleResponseStatusCode].errors.push(e);
         log.error(e);
         continue;
       }
+      
       let exampleResponseHeaders = exampleResponseValue[exampleResponseStatusCode]['headers'] || {};
       let exampleResponseBody = exampleResponseValue[exampleResponseStatusCode]['body'];
+      if (exampleResponseBody && !response.schema) {
+        let msg = `Response statusCode "${exampleResponseStatusCode}" for operation "${operation.operationId}" has response body provided in the example, ` + 
+        `however the response does not have a "schema" defined in the swagger spec.`;
+        let e = self.constructErrorObject(ErrorCodes.ResponseSchemaNotInSpec, msg);
+        result[exampleResponseStatusCode].errors.push(e);
+        log.error(e);
+        continue;
+      }
       //ensure content-type header is present
       if (!(exampleResponseHeaders['content-type'] || exampleResponseHeaders['Content-Type'])) {
         exampleResponseHeaders['content-type'] = operation.produces[0];
@@ -527,6 +608,18 @@ class SpecValidator {
       let exampleResponse = new ResponseWrapper(exampleResponseStatusCode, exampleResponseBody, exampleResponseHeaders);
       let validationResult = self.validateResponse(operation, exampleResponse);
       result[exampleResponseStatusCode] = validationResult;
+    }
+    let responseWithoutXmsExamples = Object.keys(responsesInSwagger).filter(function (statusCode) {
+      if (statusCode !== 'default') {
+        //let intStatusCode = parseInt(statusCode);
+        //if (!isNaN(intStatusCode) && intStatusCode < 400) {
+          return statusCode;
+        //}
+      }
+    });
+    if (responseWithoutXmsExamples && responseWithoutXmsExamples.length) {
+      let msg = `Following response status codes "${responseWithoutXmsExamples}" for operation "${operation.operationId}" were present in the swagger spec, ` + 
+      `however they were not present in x-ms-examples. Please provide them.`;
     }
     return result;
   }

Azure/oav

lib/specValidator.js
@@ -3,17 +3,17 @@
 
 'use strict';
 
-var Sway = require('sway'),
-  util = require('util'),
-  msRest = require('ms-rest'),
-  HttpRequest = msRest.WebResource,
+var util = require('util'),
   path = require('path'),
   fs = require('fs'),
+  Sway = require('sway'),
+  msRest = require('ms-rest'),
+  JsonRefs = require('json-refs'),
+  HttpRequest = msRest.WebResource,
   utils = require('./util/utils'),
   Constants = require('./util/constants'),
   log = require('./util/logging'),
   ResponseWrapper = require('./responseWrapper'),
-  jsonRefs = require('json-refs'),
   ErrorCodes = Constants.ErrorCodes;
 
 class SpecValidator {
@@ -47,53 +47,128 @@ class SpecValidator {
 
   findObject(what, where, actualReference, docPath) {
     let self = this;
-    let result = eval(where[what]);
+    let result = eval(`where.${what}`);
     if (!result) {
       let msg = `Object '${what}' from the given reference ` + 
       `'${actualReference}' is not found in the swagger spec '${docPath}'.`;
       let e = self.constructErrorObject('OBJECT_NOT_FOUND', msg);
-      return Promise.reject(e);
+      log.error(e);
+      throw e;
     } else {
-      return Promise.resolve(result);
+      return result;
     }
   }
 
   getDefinitionFromReference(reference) {
     let self = this;
-    if (!reference || (reference && reference.trim().length > 0)) {
+    if (!reference || (reference && reference.trim().length === 0)) {
       throw new Error('reference cannot be null or undefined and it must be a non-empty string.');
     }
     let refObj = utils.parseReferenceInSwagger(reference);
-    let result;
     if (refObj.filePath) {
       let docPath = utils.joinPath(self.specDir, refObj.filePath);
-      utils.parseJson(docPath).then(function(result) {
+      return utils.parseJson(docPath).then(function(result) {
         return self.findObject(refObj.localReference.accessorProperty, result, refObj.localReference.value, docPath);
-      }).catch(function (err) {
-        return Promise.reject(err);
       });
     } else {
       return self.findObject(refObj.localReference.accessorProperty, self.specInJson, refObj.localReference.value, self.specPath);
     }
-}
+  }
+
+  mergeResolvedAllOfObjects(source, target) {
+    let self = this;
+    if (!source || (source && typeof source !== 'object')) {
+      return Promise.reject(new Error(`source must be of type "object".`));
+    }
+    if (!target || (target && typeof target !== 'object')) {
+      return Promise.reject(new Error(`target must be of type "object".`));
+    }
+    //merge the target model's properties
+    source.properties = utils.merge(source.properties, target.properties);
+    //merge the array of required properties
+    if (target.required) {
+      if (!source.required) {
+        source.required = [];
+      }
+      source.required = [...new Set([...source.required, ...target.required])];
+    }
+    //merge x-ms-azure-resource
+    if (target['x-ms-azure-resource']) {
+      source['x-ms-azure-resource'] = target['x-ms-azure-resource'];
+    }
+    return Promise.resolve(source);
+  }
+
+  getRefDefinitionInAllOf(item) {
+    let self = this;
+    if (!item || (item && typeof item !== 'object')) {
+      return Promise.reject(new Error(`item must be of type "object".`));
+    }
+    if (item['$ref']) {
+      return Promise.resolve(self.getDefinitionFromReference(item['$ref']));
+    } else {
+      return Promise.resolve(item);
+    }
+  }
+
+  composeAllOf(model, item) {
+    let self = this;
+    return self.getRefDefinitionInAllOf(item).then(function(result) {
+      if (result && result.allOf) {
+        return self.resolveAllOf(result).then(function(res) {
+          return Promise.resolve(self.mergeResolvedAllOfObjects(model, result));
+        }).catch(function (err) {
+          log.error(err);
+          return Promise.reject(err);
+        });
+      } else {
+        return Promise.resolve(self.mergeResolvedAllOfObjects(model, result));
+      }
+    });
+  }
+
+  resolveAllOf(model) {
+    let self = this;
+    if (!model || (model && typeof model !== 'object')) {
+      return Promise.reject(new Error(`model cannot be null or undefined and must of type object.`));
+    }
+    if (model.allOf) {
+      let allOfItemPromiseFactories = model.allOf.map(function(item) {
+        return Promise.resolve(self.composeAllOf(model, item));
+      });
+      return utils.executePromisesSequentially(allOfItemPromiseFactories);
+    }
+    return Promise.resolve(model);
+  }
 
   makeModelsStricter() {
     let self = this;
     let spec = self.specInJson;
-    for (let model in spec) {
-      if (spec[model] && spec[model].allOf) {
-        spec.model.allOf.forEach(function(item) {
-          self.getDefinitionFromReference(item).then(function(result) {
-            //do something with this model.
-          }).catch(function(err) {
-            return Promise.reject(err);
-          })
-        });
+    let definitions = spec.definitions;
+    let modelNames = Object.keys(self.specInJson.definitions);
+    let modelPromiseFactories = modelNames.map(function (modelName) {
+      let model = definitions[modelName];
+      if (model && !model.additionalProperties) {
+        model.additionalProperties = false;
       }
-    }
-    return;
+      return self.resolveAllOf(model);
+    });
+    return utils.executePromisesSequentially(modelPromiseFactories);
   }
 
+  // resolveRelativePaths() {
+  //   let self = this;
+  //   let spec = self.specInJson;
+  //   let options = {
+  //     relativeBase: self.specDir,
+  //     filter: ['relative', 'remote']
+  //   };
+  //   let allRefsRemoteRelative = JsonRefs.findRefs(spec, options);
+  //   Object.keys(allRefsRemoteRelative).map(function(refName) {
+  //     let refDetails = allRefsRemoteRelative[refName];
+  //   })
+
+  // }
   updateValidityStatus(value) {
     if (!Boolean(value)) {
       this.specValidationResult.validityStatus = false;
@@ -124,10 +199,13 @@ class SpecValidator {
       self.unifyXmsPaths();
       return self;
     }).then(function () {
-      let options = {subDocPath: '#/definitions'};
-      return jsonRefs.findRefs(self.specInJson, options);
-    }).then(function(res) {
-      console.log(res);
+      return self.makeModelsStricter();
+    }).then(function() {
+      return Object.keys(self.specInJson.definitions).map(function(definitionName){
+        if (self.specInJson.definitions[definitionName].allOf) {
+          delete self.specInJson.definitions[definitionName].allOf;
+        }
+      });
     }).then(function() {
       let options = {};
       options.definition = self.specInJson;
@@ -139,9 +217,11 @@ class SpecValidator {
       self.swaggerApi = api;
       return Promise.resolve(api);
     }).catch(function (err) {
+      console.dir(err);
       let e = self.constructErrorObject(ErrorCodes.ResolveSpecError, err.message, [err]);
       self.specValidationResult.resolveSpec = e;
       log.error(`${ErrorCodes.ResolveSpecError}: ${err.message}.`);
+      log.silly(err.stack);
       return Promise.reject(e);
     });
   }
@@ -506,20 +586,36 @@ class SpecValidator {
     if (exampleResponseValue === null || exampleResponseValue === undefined || typeof exampleResponseValue !== 'object') {
       throw new Error('operation cannot be null or undefined and must be of type \'object\'.');
     }
-
+    let responsesInSwagger = {};
+    let responses = operation.getResponses().map(function(response) { 
+      responsesInSwagger[response.statusCode] = response.statusCode; 
+      return response.statusCode; 
+    });
     for (let exampleResponseStatusCode in exampleResponseValue) {
       let response = operation.getResponse(exampleResponseStatusCode);
-      result[exampleResponseStatusCode] = {errors: [], warnings: []};
+      if (responsesInSwagger[exampleResponseStatusCode]) delete responsesInSwagger[exampleResponseStatusCode];
+      result[exampleResponseStatusCode] = { errors: [], warnings: [] };
+      //have to ensure how to map negative status codes to default. There have been several issues filed in the Autorest repo, w.r.t how
+      //default is handled. While solving that issue, we may come up with some extension. Once that is finalized, we should code accordingly over here.
       if (!response) {
-        let msg = `${exampleResponseStatusCode} for operation ${operation.operationId} is provided in exampleResponseValue, however it is not present in the swagger spec.`;
-        let e = new Error(msg);
-        e.code = ErrorCodes.ResponseStatusCodeNotInSpec;
+        let msg = `Response statusCode "${exampleResponseStatusCode}" for operation "${operation.operationId}" is provided in exampleResponseValue, ` + 
+        `however it is not present in the swagger spec.`;
+        let e = self.constructErrorObject(ErrorCodes.ResponseStatusCodeNotInSpec, msg);
         result[exampleResponseStatusCode].errors.push(e);
         log.error(e);
         continue;
       }
+      
       let exampleResponseHeaders = exampleResponseValue[exampleResponseStatusCode]['headers'] || {};
       let exampleResponseBody = exampleResponseValue[exampleResponseStatusCode]['body'];
+      if (exampleResponseBody && !response.schema) {
+        let msg = `Response statusCode "${exampleResponseStatusCode}" for operation "${operation.operationId}" has response body provided in the example, ` + 
+        `however the response does not have a "schema" defined in the swagger spec.`;
+        let e = self.constructErrorObject(ErrorCodes.ResponseSchemaNotInSpec, msg);
+        result[exampleResponseStatusCode].errors.push(e);
+        log.error(e);
+        continue;
+      }
       //ensure content-type header is present
       if (!(exampleResponseHeaders['content-type'] || exampleResponseHeaders['Content-Type'])) {
         exampleResponseHeaders['content-type'] = operation.produces[0];
@@ -527,6 +623,18 @@ class SpecValidator {
       let exampleResponse = new ResponseWrapper(exampleResponseStatusCode, exampleResponseBody, exampleResponseHeaders);
       let validationResult = self.validateResponse(operation, exampleResponse);
       result[exampleResponseStatusCode] = validationResult;
+    }
+    let responseWithoutXmsExamples = Object.keys(responsesInSwagger).filter(function (statusCode) {
+      if (statusCode !== 'default') {
+        //let intStatusCode = parseInt(statusCode);
+        //if (!isNaN(intStatusCode) && intStatusCode < 400) {
+          return statusCode;
+        //}
+      }
+    });
+    if (responseWithoutXmsExamples && responseWithoutXmsExamples.length) {
+      let msg = `Following response status codes "${responseWithoutXmsExamples}" for operation "${operation.operationId}" were present in the swagger spec, ` + 
+      `however they were not present in x-ms-examples. Please provide them.`;
     }
     return result;
   }