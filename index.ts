51cc52a4dc710fe4c24a31b99cae05921877f156
Sergey Shandar <sergey.shandar@gmail.com>
2018-05-30 13:58:07 -0700

    reexport

--- index.ts
+++ index.ts
@@ -1,19 +1,20 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-import * as validate from './lib/validate'
-import * as utils from './lib/util/utils'
+// Easy to use methods from validate.ts
+export {
+  getDocumentsFromCompositeSwagger,
+  validateSpec,
+  validateCompositeSpec,
+  validateExamples,
+  validateExamplesInCompositeSpec,
+  resolveSpec,
+  resolveCompositeSpec
+} from './lib/validate'
 
-// Easy to use methods from validate.js
-export const getDocumentsFromCompositeSwagger = validate.getDocumentsFromCompositeSwagger
-export const validateSpec = validate.validateSpec
-export const validateCompositeSpec = validate.validateCompositeSpec
-export const validateExamples = validate.validateExamples
-export let validateExamplesInCompositeSpec = validate.validateExamplesInCompositeSpec
+export { executePromisesSequentially } from './lib/util/utils'
+
 export { log } from './lib/util/logging'
-export const executePromisesSequentially = utils.executePromisesSequentially
-export const resolveSpec = validate.resolveSpec
-export const resolveCompositeSpec = validate.resolveCompositeSpec
 
 // Classes
 export { SpecValidator } from './lib/validators/specValidator'
