171982ef74d2a07cc23c74afa1583e332e75ac6f
Sergey Shandar <sergey.shandar@gmail.com>
2018-07-16 20:41:14 -0700

    ValidationError

--- index.ts
+++ index.ts
@@ -19,7 +19,7 @@ export { Severity } from "./lib/util/severity"
 export { ModelValidationError } from "./lib/util/modelValidationError"
 export { ValidationResultSource } from "./lib/util/validationResultSource"
 export { getErrorsFromModelValidation } from "./lib/util/getErrorsFromModelValidation"
-export { processValidationErrors } from "./lib/util/validationError"
+export { processValidationErrors, ValidationError } from "./lib/util/validationError"
 
 export { executePromisesSequentially } from "./lib/util/utils"
 
