4033f3cc24c0d8fc4d2a2aca17933411789580b5
Vlad Barosan <vladdb@microsoft.com>
2018-07-25 11:21:52 -0700

    Update dependencies to solve security vulnerabilities

--- test/sample.ts
+++ test/sample.ts
@@ -2,22 +2,21 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 // Sample standalone script to call live validator.
-import { LiveValidator } from "../lib/validators/liveValidator"
+import { LiveValidator } from "../lib/validators/liveValidator";
 
 const options = {
-  directory: "./test/swaggers/arm-storage"
-}
-const validator = new LiveValidator(options)
+  directory: "./test/liveValidation/swaggers"
+};
+const validator = new LiveValidator(options);
 
 // tslint:disable-next-line:no-floating-promises
 validator.initialize().then(() => {
-  const reqRes = require(
-    __dirname +
+  const reqRes = require(__dirname +
     "/liveValidation/swaggers/specification/storage/resource-manager/Microsoft.Storage/" +
-    "2016-01-01/live/StorageAccounts_CheckNameAvailability.json")
+    "2016-01-01/live/StorageAccounts_CheckNameAvailability.json");
   const requestResponseObj = {
     liveRequest: reqRes.request,
     liveResponse: reqRes.response
-  }
-  validator.validateLiveRequestResponse(requestResponseObj)
-})
+  };
+  validator.validateLiveRequestResponse(requestResponseObj);
+});
