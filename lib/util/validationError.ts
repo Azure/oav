0cab5d15307651da9816542ceb686ad3679b3c09
Sergey Shandar <sergey.shandar@gmail.com>
2018-09-11 13:25:43 -0700

    @ts-common/z-schema

--- lib/util/validationError.ts
+++ lib/util/validationError.ts
@@ -5,7 +5,7 @@ import _ from "lodash"
 import { FilePosition } from "@ts-common/source-map"
 import { flatMap, fold } from "@ts-common/iterator"
 import { errorsAddFileInfo } from "./errorFileInfo"
-import { jsonSymbol, schemaSymbol } from 'z-schema';
+import { jsonSymbol, schemaSymbol } from "@ts-common/z-schema"
 
 /**
  * @class
