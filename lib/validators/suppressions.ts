195bdf50ad87b3d98ff4e51ad396fa6fa45c0cf3
Sergey Shandar <sergey.shandar@gmail.com>
2018-10-05 11:23:21 -0700

    replace `findReadMe` file.

--- lib/validators/suppressions.ts
+++ lib/validators/suppressions.ts
@@ -5,7 +5,8 @@ import * as path from "path"
 import * as fs from "fs"
 import * as md from "@ts-common/commonmark-to-markdown"
 import * as amd from "@ts-common/azure-openapi-markdown"
-import { isArray } from '@ts-common/iterator';
+import { isArray } from "@ts-common/iterator"
+import { findReadMe } from "@ts-common/azure-openapi-markdown"
 
 export const getSuppressions = (specPath: string): undefined | amd.Suppression => {
   // find readme.md
@@ -24,19 +25,4 @@ export const getSuppressions = (specPath: string): undefined | amd.Suppression =
     return undefined
   }
   return suppression
-}
-
-const findReadMe = (dir: string): string | undefined => {
-  dir = path.resolve(dir)
-  while (true) {
-    const fileName = path.join(dir, "readme.md")
-    if (fs.existsSync(fileName)) {
-      return fileName
-    }
-    const newDir = path.dirname(dir)
-    if (newDir === dir) {
-      return undefined
-    }
-    dir = newDir
-  }
 }
