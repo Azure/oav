5da7f0bf45d989c6447d1fb2ddd370aef5140400
Sergey Shandar <sergey.shandar@gmail.com>
2018-10-12 19:39:22 -0700

    improved source map test

--- test/sourceMapTests.ts
+++ test/sourceMapTests.ts
@@ -6,9 +6,11 @@ import * as assert from "assert"
 
 describe("sourceMap", () => {
   it("INVALID_TYPE", async () => {
-    const result = await validateExamples(
+    const file =
       // tslint:disable-next-line:max-line-length
-      "./test/modelValidation/swaggers/specification/source-map/Microsoft.ADHybridHealthService/stable/2014-01-01/ADHybridHealthService.json",
+      "./test/modelValidation/swaggers/specification/source-map/Microsoft.ADHybridHealthService/stable/2014-01-01/ADHybridHealthService.json"
+    const result = await validateExamples(
+      file,
       undefined,
       {
         consoleLogLevel: "off"
@@ -17,7 +19,7 @@ describe("sourceMap", () => {
     assert.strictEqual(result.length, 1)
     const result0 = result[0]
     const e = result0.errorDetails as any
-    assert.notStrictEqual(e.url, undefined)
+    assert.strictEqual(e.url, file)
     assert.deepStrictEqual(e.position, { line: 76, column: 11 })
   })
 })
