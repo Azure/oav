c6b3b9bed2bb2aeced3e76fe07027cab4c3b0261
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2018-10-15 16:29:12 -0700

    Provide schema information for all Schema Objects. (#341)
    * update all schema objects
    
    * 0.7.10
    
    * changelog
    
    * unit test
    
    * address comments

---
+++ test/errorInfoTests.ts
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+import { validateExamples } from "../lib/validate"
+import * as assert from "assert"
+
+describe("error info", () => {
+  it("error info should contain url to file", async () => {
+    const result = await validateExamples(
+      "./test/modelValidation/swaggers/specification/errorInfo/apimusers.json",
+      undefined,
+      {
+        consoleLogLevel: "off"
+      }
+    )
+    assert.strictEqual(result.length, 2)
+    const r = result[0].errorDetails as any
+    assert.strictEqual(
+      r.url, "./test/modelValidation/swaggers/specification/errorInfo/apimusers.json")
+  })
+})
