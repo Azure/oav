08f1e617f48c903f43fc1efc93740110d575298a
Sergey Shandar <sergey.shandar@gmail.com>
2019-02-06 15:52:35 -0800

    copyright

--- lib/util/jsonRefs.ts
+++ lib/util/jsonRefs.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
 import * as jsonRefs from "json-refs"
 import * as sm from "@ts-common/string-map"
 
