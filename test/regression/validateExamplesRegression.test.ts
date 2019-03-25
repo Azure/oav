8b3d1970622546fc118876fbdcc39ef2fa90f627
Billy Trend <witrend@microsoft.com>
2019-03-25 12:52:15 +0000

    Test snap update

--- test/regression/validateExamplesRegression.test.ts
+++ test/regression/validateExamplesRegression.test.ts
@@ -5,12 +5,11 @@ import * as path from "path"
 import * as validate from "../../lib/validate"
 
 describe("@regression validateExamples should not regress for file", () => {
-  const specPaths = glob.sync(
-    path.join(__dirname, "azure-rest-api-specs/specification/**/*.json"),
-    {
+  const specPaths = glob
+    .sync(path.join(__dirname, "azure-rest-api-specs/specification/**/*.json"), {
       ignore: ["azure-rest-api-specs/specification/**/examples/*"]
-    }
-  )
+    })
+    .slice(0, 5)
 
   test.each(specPaths)(
     "'%s'",
