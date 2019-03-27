fc81447287e4123d477be3328fead917fd8d7670
Billy Trend <witrend@microsoft.com>
2019-03-27 10:39:08 +0000

    Add latest spec version tag to tests

--- test/regression/validateExamplesRegression.test.ts
+++ test/regression/validateExamplesRegression.test.ts
@@ -1,10 +1,11 @@
 import glob = require("glob")
+import * as _ from "lodash"
 import md5File = require("md5-file")
 import * as path from "path"
 
 import * as validate from "../../lib/validate"
 
-describe("@regression validateExamples should not regress for file", () => {
+describe("validateExamples should not regress for file", () => {
   const specPaths = glob.sync(
     path.join(__dirname, "azure-rest-api-specs/specification/**/*.json"),
     {
@@ -12,13 +13,37 @@ describe("@regression validateExamples should not regress for file", () => {
     }
   )
 
-  test.each(specPaths)(
-    "'%s'",
+  const versionRegex = /(?:\/)[0-9-]*[^\/]*/
+  const rpRegex = /(?:\/)Microsoft.[^\/]*/
+  const latestForEachRp = (_.chain(specPaths)
+    .filter(p => p.includes("stable")) // only look at stable specs
+    .filter(p => p.match(rpRegex))
+    .groupBy((p: string) => p.match(rpRegex)![0]) // group paths by rp
+    .entries()
+    .map((entry: [string, string[]]) =>
+      // for each rp get latest version
+      _.chain(entry[1])
+        .filter(p => p.match(versionRegex))
+        .sortBy((p: string) => p.match(versionRegex)![0])
+        .last()
+        .value()
+    )
+    .toArray()
+    .value() as unknown) as string[]
+
+  test.each(specPaths.map(path => [path, latestForEachRp.includes(path) ? "@latestVersion" : ""]))(
+    "'%s' %s",
     async file => {
       try {
-        const hash = await new Promise(resolve => md5File(file, resolve))
+        const hash = await new Promise(resolve =>
+          md5File(file, (e, h) => {
+            if (e) {
+              throw e
+            }
+            resolve(h)
+          })
+        )
         expect(hash).toMatchSnapshot("input file hash")
-
         const result = await validate.validateExamples(file, undefined, {
           pretty: true
         })
