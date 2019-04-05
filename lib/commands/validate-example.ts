f5c5aeed1f6bff498a7142c4b46f79234d19496d
Sergey Shandar <sergey.shandar@gmail.com>
2019-04-05 11:27:21 -0700

    changelog

--- lib/commands/validate-example.ts
+++ lib/commands/validate-example.ts
@@ -35,10 +35,10 @@ export async function handler(argv: yargs.Arguments): Promise<void> {
     }
     if (specPath.match(/.*composite.*/gi) !== null) {
       const result = await validate.validateExamplesInCompositeSpec(specPath, vOptions)
-      return iterator.flat(result).some() ? 1 : 0
+      return iterator.flat(result).some(() => true) ? 1 : 0
     } else {
       const result = await validate.validateExamples(specPath, operationIds, vOptions)
-      return iterator.some(result) ? 1 : 0
+      return iterator.some(result, () => true) ? 1 : 0
     }
   })
 }
