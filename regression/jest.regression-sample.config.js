4bbf17de1db24c7f771cb49f42b0f069ca2f168e
Billy Trend <witrend@microsoft.com>
2019-05-06 18:52:51 -0700

    Move config to separate files

---
+++ regression/jest.regression-sample.config.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+const mainConfig = require("../jest.config")
+
+/** @type {jest.InitialOptions} */
+module.exports = {
+  ...mainConfig,
+  testMatch: ["**/regression/**/validateExamplesRegression_latestOnly.test.ts"]
+}
