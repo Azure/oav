d7f69d59cf9a6d27abea56ccf80228db31b5b772
Wenhao Zhang <sjtu_zhangwh@163.com>
2020-08-14 15:56:26 +0800

    Export more methods from oav for validation in unified pipeline (#523)
    * Export more methods from oav for validation in unified pipeline
    
    * minor
    
    * add change log and bump version
    
    Co-authored-by: t-zhangw <codespace@72c482c53b75>

--- index.ts
+++ index.ts
@@ -13,6 +13,7 @@ export {
   resolveCompositeSpec
 } from "./lib/validate"
 
+export { CommonError } from "./lib/util/commonError"
 export { BaseValidationError } from "./lib/util/baseValidationError"
 export { Severity } from "./lib/util/severity"
 export { ModelValidationError } from "./lib/util/modelValidationError"
@@ -35,6 +36,8 @@ export {
 
 // Classes
 export { SpecValidator } from "./lib/validators/specValidator"
+export { ModelValidator } from "./lib/validators/modelValidator"
+export { SemanticValidator } from "./lib/validators/semanticValidator"
 export {
   LiveValidator,
   RequestResponsePair,
@@ -48,6 +51,7 @@ export {
   ValidateOptions
 } from "./lib/validators/liveValidator"
 export { SpecResolver } from "./lib/validators/specResolver"
+export { log } from "./lib/util/logging"
 
 // Constants
 export const Constants = C
