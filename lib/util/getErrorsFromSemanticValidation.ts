fddd79f4f0f040b06e319b882aada18e4e061878
t-zhangw <codespace@72c482c53b75>
2020-09-07 05:28:05 +0000

    New function for unified pipeline to extract semantic validation inner error.

--- lib/util/getErrorsFromSemanticValidation.ts
+++ lib/util/getErrorsFromSemanticValidation.ts
@@ -45,3 +45,43 @@ export const getErrorsFromSemanticValidation = (
     return [...acc, ...semanticErrors]
   }, new Array<SemanticValidationError>())
 }
+
+/**
+ * From the raw validator engine semantic validation results process errors to be served.
+ * This function is used only for unified pipeline that would serialize all errors
+ */
+export const getErrorsFromSemanticValidationForUnifiedPipeline = (
+  validationResult: SpecValidationResult & ValidationResult
+): SemanticValidationError[] => {
+  if (!validationResult.validateSpec || !validationResult.validateSpec.errors) {
+    return []
+  }
+
+  return validationResult.validateSpec.errors
+    .reduce((acc, rawError) => {
+      const serializedErrors: any[] = serializeErrorsForUnifiedPipeline(
+        rawError.inner || rawError,
+        []
+      )
+
+      // process serialized errors
+      const semanticErrors: SemanticValidationError[] = serializedErrors.map(serializedError => {
+        const severity = errorCodeToErrorMetadata(serializedError.code).severity
+        const semanticError: SemanticValidationError = {
+          source: ValidationResultSource.GLOBAL,
+          code: serializedError.code,
+          details: serializedError,
+          path: rawError["json-path"],
+          severity
+        }
+        return semanticError
+      })
+      return [...acc, ...semanticErrors]
+    }, new Array<SemanticValidationError>())
+    .filter(
+      it =>
+        it.code !== "ANY_OF_MISSING" &&
+        it.code !== "ONE_OF_MISSING" &&
+        it.code !== "ONR_OF_MULTIPLE"
+    )
+}
