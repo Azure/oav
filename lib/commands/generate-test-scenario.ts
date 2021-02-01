33adf5ddb576a69c3c09b9af50c03c6070514eaa
Phoenix He <tih@microsoft.com>
2021-02-01 14:00:59 +0800

    Support generate file (#575)

--- lib/commands/generate-test-scenario.ts
+++ lib/commands/generate-test-scenario.ts
@@ -4,7 +4,11 @@
 /* eslint-disable id-blacklist */
 
 import globby from "globby";
+import { dirname } from "path";
 import * as yargs from "yargs";
+import { inversifyGetInstance } from "../inversifyUtils";
+import { TestRecordingLoader } from "../testScenario/gen/testRecordingLoader";
+import { RequestTracking, TestScenarioGenerator } from "../testScenario/gen/testScenarioGenerator";
 import { getAutorestConfig } from "../util/getAutorestConfig";
 
 export const command = "generate-test-scenario";
@@ -42,10 +46,31 @@ export async function handler(argv: yargs.Arguments): Promise<void> {
 
   const recordingFilePaths = await globby(recording);
   const autorestConfig = await getAutorestConfig(argv, readmeMd);
+  const swaggerFilePaths: string[] = autorestConfig["input-file"];
   console.log("input-file:");
-  console.log(autorestConfig["input-file"]);
+  console.log();
   console.log("recording-file:");
   console.log(recordingFilePaths);
   console.log("output-file:");
   console.log(`\t${output}\n`);
+
+  const generator = TestScenarioGenerator.create({
+    useJsonParser: false,
+    checkUnderFileRoot: false,
+    fileRoot: dirname(readmeMd),
+    swaggerFilePaths,
+  });
+
+  await generator.initialize();
+
+  const recordingLoader = inversifyGetInstance(TestRecordingLoader, {});
+  const trackingList: RequestTracking[] = [];
+  for (const filePath of recordingFilePaths) {
+    // console.log(filePath);
+    const tracking = await recordingLoader.load(filePath);
+    trackingList.push(tracking);
+  }
+  await generator.generateTestDefinition(trackingList, output);
+
+  await generator.writeGeneratedFiles();
 }
