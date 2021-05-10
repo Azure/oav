1a7182611c5267a1c54069448f569ed18aa4683b
ruowan <ruowan@microsoft.com>
2021-05-10 09:45:52 +0000

    update runtime error code

--- lib/testScenario/reportGenerator.ts
+++ lib/testScenario/reportGenerator.ts
@@ -409,7 +409,7 @@ export class ReportGenerator {
       detail: this.dataMasker.jsonStringify(it.response.body),
     };
     const responseObj = this.dataMasker.jsonParse(it.response.body);
-    ret.code = "RUNTIME_ERROR";
+    ret.code = `RUNTIME_ERROR_${it.response.statusCode}`;
     ret.message = responseObj?.error?.message;
     return ret;
   }
