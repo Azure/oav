53413b8a80ad7ef6d51dd0d6b098f0a9b6508999
jianye xi <jianyxi@microsoft.com>
2021-07-13 10:54:13 +0800

    load env

--- lib/commands/run-test-scenario.ts
+++ lib/commands/run-test-scenario.ts
@@ -19,6 +19,8 @@ export const command = "run-test-scenario <test-scenario>";
 
 export const describe = "newman runner run test scenario file.";
 
+export const testScenarioEnvKey = "TEST_SCENARIO_JSON_ENV";
+
 /**
  * UploadBlob true. Upload generated file and result to azure blob storage. connection string is passed by `process.env.blobConnectionString`
  * Upload files:
@@ -93,6 +95,9 @@ export async function handler(argv: yargs.Arguments): Promise<void> {
     let env: any = {};
     if (argv.e !== undefined) {
       env = JSON.parse(fs.readFileSync(argv.e).toString());
+    }
+    else if (process.env[testScenarioEnvKey]) {
+      env = JSON.parse(process.env[testScenarioEnvKey] as string);
     }
     // fileRoot is the nearest common root of all swagger file paths
     const fileRoot = path.dirname(swaggerFilePaths[0]);
