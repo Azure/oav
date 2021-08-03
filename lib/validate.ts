a45480e9f50a5ac440871b9e60cbc71553d3ef4b
Ligengxin <Ligengxin96@gmail.com>
2021-08-03 13:38:43 +0800

    Disable checkUnderFileRoot

--- lib/validate.ts
+++ lib/validate.ts
@@ -307,6 +307,7 @@ export async function validateTrafficAgainstSpec(
       o.consoleLogLevel = log.consoleLogLevel;
       o.logFilepath = log.filepath;
       const liveValidationOptions = {
+        checkUnderFileRoot: false,
         directory: specFileDirectory,
         swaggerPathsPattern: [swaggerPathsPattern],
         git: {
