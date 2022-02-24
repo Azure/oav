779ac987de43be25ea08ea6a40efcf78aefe4940
KeYu <1160746208@qq.com>
2022-02-24 16:35:46 +0800

    add test for apiScenarioLoader (coverage: 79%)

--- test/apiScenario/apiScenarioLoaderTests.ts
+++ test/apiScenario/apiScenarioLoaderTests.ts
@@ -2,7 +2,7 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 /* tslint:disable:no-console max-line-length*/
-
+import * as assert from "assert";
 import path from "path";
 import {
   PostmanCollectionGenerator,
@@ -10,50 +10,101 @@ import {
 } from "../../lib/apiScenario/postmanCollectionGenerator";
 import { inversifyGetInstance } from "../../lib/inversifyUtils";
 
-const specPath = `${__dirname}/swaggers/specification`;
+const specPath = `${__dirname}/swaggers/specification/appplatform/resource-manager/Microsoft.AppPlatform/preview/2020-11-01-preview/scenarios`;
+
+const testAPI = async (scenariosPath: string, envData: object) => {
+  const scenarioFilePath = path.resolve(scenariosPath);
+  const fileRoot = path.dirname(scenariosPath);
+  const opt: PostmanCollectionGeneratorOption = {
+    name: path.basename(scenarioFilePath),
+    scenarioDef: scenarioFilePath,
+    fileRoot: fileRoot,
+    checkUnderFileRoot: false,
+    generateCollection: true,
+    useJsonParser: false,
+    runCollection: true,
+    env: envData,
+    outputFolder: "generated",
+    markdownReportPath: undefined,
+    junitReportPath: undefined,
+    eraseXmsExamples: false,
+    eraseDescription: false,
+    enableBlobUploader: false,
+    blobConnectionString: process.env.blobConnectionString || "",
+    baseUrl: "https://management.azure.com",
+    validationLevel: "validate-request-response",
+    skipCleanUp: undefined,
+    from: undefined,
+    to: undefined,
+    runId: undefined,
+    verbose: false,
+  };
+  const generator = inversifyGetInstance(PostmanCollectionGenerator, opt);
+  await generator.GenerateCollection();
+};
 
 describe("API Scenario Loader", () => {
-  describe("Path validation - ", () => {
-    it("should pass when path parameter has forward slashes", async () => {
+  describe("swagger", () => {
+    it("should fail when method don't have operationId", async () => {
       try {
-        const scenariosPath = `${specPath}/signal/resource-manager/Microsoft.SignalRService/preview/2021-06-01-preview/scenarios/basic.yaml`;
-        const scenarioFilePath = path.resolve(scenariosPath);
-        const fileRoot = path.dirname(scenariosPath);
-        const env = {
-          subscriptionId: "00000000-0000-0000-0000-000000000000",
-          location:
-            "https://management.azure.com/subscriptions/subid/providers/Microsoft.SignalRService/...pathToOperationResult...",
-        };
-        const opt: PostmanCollectionGeneratorOption = {
-          name: path.basename(scenarioFilePath),
-          scenarioDef: scenarioFilePath,
-          fileRoot: fileRoot,
-          checkUnderFileRoot: false,
-          generateCollection: true,
-          useJsonParser: false,
-          runCollection: true,
-          env: env,
-          outputFolder: "generated",
-          markdownReportPath: undefined,
-          junitReportPath: undefined,
-          eraseXmsExamples: false,
-          eraseDescription: false,
-          enableBlobUploader: false,
-          blobConnectionString: process.env.blobConnectionString || "",
-          baseUrl: "https://management.azure.com",
-          validationLevel: "validate-request-response",
-          skipCleanUp: undefined,
-          from: undefined,
-          to: undefined,
-          runId: undefined,
-          verbose: false,
-        };
-        const generator = inversifyGetInstance(PostmanCollectionGenerator, opt);
-        await generator.GenerateCollection();
+        const scenariosPath = `${specPath}/undefinedOperationId.yaml`;
+        await testAPI(scenariosPath, {});
       } catch (err) {
-        const message = `test error: ${err.message}, ${JSON.stringify(err)}`;
-        console.error(message);
+        assert.strictEqual(err.message.substring(0, 38), "OperationId is undefined for operation");
       }
-    }, 300000);
+    });
+
+    it("should fail when swagger has duplicated operationId", async () => {
+      try {
+        const scenariosPath = `${specPath}/duplicatedOperationId.yaml`;
+        await testAPI(scenariosPath, {});
+      } catch (err) {
+        assert.strictEqual(err.message.split(": ")[0], "Duplicated operationId Services_Get");
+      }
+    });
+
+    it("should fail when xMsExamples don't use $ref property", async () => {
+      try {
+        const scenariosPath = `${specPath}/xMsExamplesNoRef.yaml`;
+        await testAPI(scenariosPath, {});
+      } catch (err) {
+        assert.strictEqual(err.message, "Example doesn't use $ref: Services_Get");
+      }
+    });
+  });
+
+  describe("API Scenario", () => {
+    it("should fail when step has invalid operationId", async () => {
+      try {
+        const scenariosPath = `${specPath}/stepOperationId.yaml`;
+        await testAPI(scenariosPath, {});
+      } catch (err) {
+        assert.strictEqual(err.message.split(": ")[1].substring(0, 23), "Operation not found for");
+      }
+    });
+
+    it("should fail when step has invalid file path", async () => {
+      try {
+        const scenariosPath = `${specPath}/filePath.yaml`;
+        await testAPI(scenariosPath, {});
+      } catch (err) {
+        assert.strictEqual(
+          err.message.split(": ")[2].substring(0, 25),
+          "no such file or directory"
+        );
+      }
+    });
+
+    it("should fail when missing requiredVariables in env", async () => {
+      try {
+        const scenariosPath = `${specPath}/basic.yaml`;
+        await testAPI(scenariosPath, {});
+      } catch (err) {
+        assert.strictEqual(
+          err.message.substring(0, 44),
+          "Missing required variable 'customDomainName'"
+        );
+      }
+    });
   });
 });
