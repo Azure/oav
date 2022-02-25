1160229dd77affe1d48e5d8ab64e8db2067fec74
Xiaoxu Qi <xiaoxuqi@microsoft.com>
2022-02-26 03:31:16 +0800

    test

--- test/liveValidatorDataPlaneTests.ts
+++ test/liveValidatorDataPlaneTests.ts
@@ -29,7 +29,7 @@ describe("LiveValidator for data-plane", () => {
     it("should load operationSpecMapper during initialization", async () => {
       let specPath =
         "test/liveValidation/swaggers/specification/cosmos-db/data-plane/Microsoft.Tables/preview/2019-02-02/table.json";
-      let trafficPath = "test/liveValidation/payloads/coveragetest/";
+      let trafficPath = "test/liveValidation/payloads/dataplane/";
       specPath = path.resolve(process.cwd(), specPath);
       trafficPath = path.resolve(process.cwd(), trafficPath);
       const validator = new TrafficValidator(specPath, trafficPath);
@@ -51,11 +51,15 @@ describe("LiveValidator for data-plane", () => {
       const validator = new TrafficValidator(specPath, trafficPath);
       await validator.initialize();
       await validator.validate();
-      expect(validator.coverageResult.size).toEqual(1);
-      expect(validator.coverageResult.get(keyPath)).toEqual(1.0/14.0); 
+      expect(validator.coverageResult.size).toEqual(2);
+      for (const key of validator.coverageResult.keys()) {
+        console.log(`${key} ${validator.coverageResult.get(key)}`);
+      }
+      expect(validator.coverageResult.get(keyPath)).toEqual(2.0/14.0); 
+      expect(validator.coverageData.length).toEqual(2);
       for (let i of validator.coverageData) {
         if (i.spec === keyPath) {
-          expect(i.coveredOperaions).toEqual(1);
+          expect(i.coveredOperaions).toEqual(2);
           expect(i.totalOperations).toEqual(14);
         }
       }
