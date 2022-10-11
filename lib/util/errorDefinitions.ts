8ab1d5a0dda983c6bd3ad75093a61cd2f9e55d0b
xiaoxuqi <xiaoxuqi@microsoft.com>
2022-10-11 17:36:29 +0800

    update snapshot

--- lib/util/errorDefinitions.ts
+++ lib/util/errorDefinitions.ts
@@ -330,15 +330,15 @@ export const apiValidationRuntimeErrors = {
 export const roundTripValidationErrors = {
   ROUNDTRIP_INCONSISTENT_PROPERTY: {
     severity: Severity.Critical,
-    message: strTemplate`The property's value "${"getValue"}" in the GET response is different from what was set "${"putValue"}" in the preceding PUT request.`,
+    message: strTemplate`The property's value '${"getValue"}' in the GET response is different from what was set '${"putValue"}' in the preceding PUT request.`,
   },
   ROUNDTRIP_MISSING_PROPERTY: {
     severity: Severity.Critical,
-    message: strTemplate`The property "${"property"}" is present in the PUT request but is either never returned in the GET response or is returned with a null value.`,
+    message: strTemplate`The property '${"property"}' is present in the PUT request but is either never returned in the GET response or is returned with a null value.`,
   },
   ROUNDTRIP_ADDITIONAL_PROPERTY: {
     severity: Severity.Critical,
-    message: strTemplate`The property "${"property"}" is returned in the GET response, but it is not declared in the PUT request.`,
+    message: strTemplate`The property '${"property"}' is returned in the GET response, but it is not declared in the PUT request.`,
   },
 };
 
