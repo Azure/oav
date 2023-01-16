ea0ad6500412c50b51ebb56d58aa16a21372fe35
Lei Ni <leni@microsoft.com>
2023-01-16 12:02:00 +0800

    test case

--- test/trafficValidatorTests.ts
+++ test/trafficValidatorTests.ts
@@ -1,14 +1,10 @@
-import * as path from "path";
 import { TrafficValidator } from "../lib/swaggerValidator/trafficValidator";
 
 describe("TrafficValidator", () => {
   it("validate data-plane traffic", async () => {
     const validator = new TrafficValidator(
-      path.resolve(
-        process.cwd(),
-        "./test/liveValidation/swaggers/specification/cosmos-db/data-plane/Microsoft.Tables"
-      ),
-      path.resolve(process.cwd(), "./test/liveValidation/payloads/dataplane/")
+      "test/liveValidation/swaggers/specification/cosmos-db/data-plane/Microsoft.Tables",
+      "test/liveValidation/payloads/dataplane/"
     );
     await validator.initialize();
     const result = {} as any;
