628af7d53fda96fb36f257fc658c63a49aa95fcf
Scott Beddall (from Dev Box) <scbedd@microsoft.com>
2023-12-08 18:20:17 -0800

    update to include the root dir

--- regression/jest.regression.config.js
+++ regression/jest.regression.config.js
@@ -5,7 +5,7 @@ const mainConfig = require("../jest.config");
 /** @type {jest.InitialOptions} */
 module.exports = {
   ...mainConfig,
-  snapshotSerializers: ["../backSlashSnapshotSerializer.js"],
+  snapshotSerializers: ["<rootDir>/../backSlashSnapshotSerializer.js"],
   testMatch: ["**/regression/**/*.test.ts"],
   setupFilesAfterEnv: ["<rootDir>/../jest.setup-file.js"]
 };
