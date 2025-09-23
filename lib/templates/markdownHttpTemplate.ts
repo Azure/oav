13a47c92abd203e460bf7a31c20d12035c6304c3
Mike Harder <mharder@microsoft.com>
2025-09-23 22:48:53 +0000

    Replace dependency `uuid` with built-in `crypto.randomUUID()`

--- lib/templates/markdownHttpTemplate.ts
+++ lib/templates/markdownHttpTemplate.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+import { randomUUID } from "crypto";
 import { keys, toArray } from "@azure-tools/openapi-tools-common";
-import * as uuid from "uuid";
 
 import { HttpTemplate, Request, Response, Responses } from "./httpTemplate";
 
@@ -104,7 +104,7 @@ ${this.getRequestBody()}
     if (!responseType) {
       responseType = "Response";
     }
-    const responseGuid = uuid.v4();
+    const responseGuid = randomUUID();
     const responseTemplate = `
 ## ${responseType}
 
