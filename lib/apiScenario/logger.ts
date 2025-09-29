47fd99c64ec3ad7708a6d0d79d55969d705d61ff
Mike Harder <mharder@microsoft.com>
2025-09-29 11:57:00 -0700

    bump winston from 3.16.0 to 3.17.0 (#1165)
    - adds workaround to (bugged?) breaking change in typings

--- lib/apiScenario/logger.ts
+++ lib/apiScenario/logger.ts
@@ -8,7 +8,9 @@ export const logger = winston.createLogger({
         winston.format.colorize({
           message: true,
         }),
-        winston.format.printf((info) => info.message)
+        // Likely a bug that 'info.message' has type 'unknown', but it's easy to workaround by converting to string
+        // https://github.com/winstonjs/winston/issues/2528
+        winston.format.printf((info) => String(info.message))
       ),
     }),
   ],
