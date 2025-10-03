a8a1402605e0dfaf74068adff86d6659d061f28f
Mike Harder <mharder@microsoft.com>
2025-10-03 06:30:56 +0000

    simplify assertions

--- test/readOnlyValidationTests.ts
+++ test/readOnlyValidationTests.ts
@@ -1,5 +1,3 @@
-import * as assert from "assert";
-
 import * as validate from "../lib/validate";
 
 const specPath = `test/modelValidation/swaggers/specification/validateReadOnly/cdn.json`;
@@ -9,40 +7,23 @@ describe("Read Only properties", () => {
     const result = await validate.validateExamples(specPath, "Profiles_Create", {
       consoleLogLevel: "off",
     });
-    assert.strictEqual(
-      result.length,
-      1,
-      `swagger "${specPath}" with operation should contain 1 model validation errors.`
-    );
-    assert.strictEqual(
-      result[0].code === "READONLY_PROPERTY_NOT_ALLOWED_IN_REQUEST",
-      true,
-      `swagger "${specPath}" should throw an error with code READONLY_PROPERTY_NOT_ALLOWED_IN_REQUEST`
-    );
-    assert.strictEqual(
-      result[0].message,
-      'ReadOnly property "provisioningState" cannot be sent in the request'
-    );
+
+    expect(result).toMatchObject([
+      {
+        code: "READONLY_PROPERTY_NOT_ALLOWED_IN_REQUEST",
+        message: 'ReadOnly property "provisioningState" cannot be sent in the request',
+      },
+    ]);
   });
 
   it("for a number value discriminator should throw 2 errors - readonly properties not allowed in request and invalid type", async () => {
     const result = await validate.validateExamples(specPath, "Profiles_Update", {
       consoleLogLevel: "off",
     });
-    assert.strictEqual(
-      result.length,
-      2,
-      `swagger "${specPath}" with operation should contain 2 model validation errors.`
-    );
-    assert.strictEqual(
-      result[0].code === "INVALID_TYPE",
-      true,
-      `swagger "${specPath}" should throw an error with code READONLY_PROPERTY_NOT_ALLOWED_IN_REQUEST`
-    );
-    assert.strictEqual(
-      result[1].code === "READONLY_PROPERTY_NOT_ALLOWED_IN_REQUEST",
-      true,
-      `swagger "${specPath}" should throw an error with code READONLY_PROPERTY_NOT_ALLOWED_IN_REQUEST`
-    );
+
+    expect(result).toMatchObject([
+      { code: "INVALID_TYPE" },
+      { code: "READONLY_PROPERTY_NOT_ALLOWED_IN_REQUEST" },
+    ]);
   });
 });
